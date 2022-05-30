f07688855275dbe306f932772d86b4e62a37c32b
Rodrigo Ferreira <rodrigoferreira.mkt@gmail.com>
2022-05-30 19:58:40 -0300

    fisrt commit - Projeto Original

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Rodrigo Ferreira da Cunha");
+		
+		Conta cc = new ContaCorrente(cliente);
+		Conta cp = new ContaPoupanca(cliente);
+		
+		cc.depositar(100);
+		cc.transferir(100, cp);
+		
+		cc.imprimirExtrato();
+		cp.imprimirExtrato();
+	}
+
+}
